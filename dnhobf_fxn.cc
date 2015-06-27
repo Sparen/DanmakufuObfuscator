5446d8fcdf2839dc1ea2d99f7dbac7cf395f6c0c
Andrew Fan <andrewsuicune@gmail.com>
2015-06-27 16:43:38 -0400

    Now supports removal of tabs and new lines. Untested on larger files

--- dnhobf_fxn.cc
+++ dnhobf_fxn.cc
@@ -25,6 +25,7 @@ int ObfuscateA1(std::string infile){
 int ObfuscateA2(std::string infile){
   RemoveBlockComments(infile);
   RemoveSingleLineComments(infile);
+  RemoveNewLinesAndTabs(infile);
   return 0;
 }
 
@@ -133,6 +134,51 @@ int RemoveBlockComments(std::string infile){
 	  if(orig.eof()){satisfied = true;}//get will never throw an EOF
 	}
       }
+      if(orig.fail() || tmp.fail()){
+	cerr << "Error: Internal Logic Failure or File Stream Corruption" << endl;
+	exit(EXIT_FAILURE);
+      }
+    }else{
+      tmp.put(c);
+    }
+  }
+
+  orig.close();
+  tmp.close();
+
+  remove(infile.c_str());//remove original
+  rename(newfile.c_str(), infile.c_str());//replace original with temp
+
+  return 0;
+}
+
+//Replaces all instances of new lines and tabs (\n \r \t) with single spaces
+int RemoveNewLinesAndTabs(std::string infile){
+  cout << "~~~Now Removing New Lines and Tabs~~~" << endl;
+  char c;
+  string newfile = infile + ".temp";
+
+  fstream orig;
+  orig.open(infile.c_str(), std::fstream::in);
+  if(orig.fail()){
+    cerr << "Error: Failed to open Data File" << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  fstream tmp;
+  //cout << "Creating Temporary File with name " << newfile << endl;//debug
+  tmp.open(newfile.c_str(), std::fstream::in | std::fstream::out | std::fstream::app);
+  if(tmp.fail()){
+    cerr << "Error: Failed to open Temporary File" << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  while(!orig.eof()){
+    orig.get(c);
+    if(orig.eof()){break;}//get will never throw an EOF, and will duplicate the last character instead.
+    //cout << c; //debug
+    if(c=='\n' || c=='\r' || c=='\t'){
+      tmp.put(' '); //replace with white space
       if(orig.fail() || tmp.fail()){
 	cerr << "Error: Internal Logic Failure or File Stream Corruption" << endl;
 	exit(EXIT_FAILURE);
--- dnhobf_fxn.h
+++ dnhobf_fxn.h
@@ -10,5 +10,6 @@ int ObfuscateA1(std::string infile);
 int ObfuscateA2(std::string infile);
 int RemoveSingleLineComments(std::string infile);
 int RemoveBlockComments(std::string infile);
+int RemoveNewLinesAndTabs(std::string infile);
 
 #endif
